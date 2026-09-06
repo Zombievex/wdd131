b131eccdc19336e820b3b80a14e330b7d156a840
Zombievex <dnk7z2fcfd@privaterelay.appleid.com>
2026-09-05 18:31:52 -0600

    Pre-test w1

---
+++ Scripts/get_dates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+document.getElementById("lastModified").textContent = document.lastModified;
